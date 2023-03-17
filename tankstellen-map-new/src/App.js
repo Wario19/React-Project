17a4d7354ab687416842251e41ceba9d576ab4f4
Wario19 <konstantin.zerlauth@gmx.at>
2023-03-17 16:37:57 +0100

    Marker Positions (not working)

--- tankstellen-map-new/src/App.js
+++ tankstellen-map-new/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
 
+const positions = [{longitude:"47.41366", latitude:"9.74237"},{longitude:"47.41366", latitude:"9.74237"},{longitude:"47.41366", latitude:"9.74237"},];
+
 function App() {
   return (
     <div className="App">
-      <h1>Tankstellen Karte</h1>
+      <h1>Gas Station Map</h1>
       <div style={{height:"400px",height:"400px",backgroundColor:'green'}}>
-        <Map/>
+        <Map props={positions}/>
       </div>
     </div>
   );
--- tankstellen-map-new/src/components/Map.js
+++ tankstellen-map-new/src/components/Map.js
@@ -1,24 +1,36 @@
 import React from 'react'
-import { MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+
+ 
+export default function Map(props) {
+
+    const positions = [props.longitude, props.latitude];
+
+    function setMarker([positions]) {
+        const output = [];
+        const pos = [positions.longitude, positions.latitude];
+        positions.forEach(element => {
+            <Marker position={pos}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+            output.push();
+        });
+
+        return output;
+    }
 
 
-export default function Map() {
     return (
         <>
-            <MapContainer style={{height:"650px"}} center={[47.41366, 9.74237]} zoom={14} scrollWheelZoom={false}>
+            <MapContainer style={{ height: "650px" }} center={[47.41366, 9.74237]} zoom={14} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+            <setMarker/>
             </MapContainer>
-
         </>
-
-
     )
 }
